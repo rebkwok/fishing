7165a8b90978fe6bc5004580546da944c5568e01
Becky Smith <rebkwok@gmail.com>
2014-11-15 11:17:14 +0000

    Creating artwork for fishing game

--- README.md
+++ README.md
@@ -1,40 +1,7 @@
 #Fishing
 --------------------
 
-Prints colourful fish
+A game about fishing and maths
 
 -----------------------
-
-First ```pip install colorama```
-
-From command line:
-
-```
-python3 fishing.py
-```
-
-will print a random coloured fish.
-
-
-Or from interpreter:
-
-```
-from fishing import fish
-
-fish()
-```
-
-fish takes optional parameters index (0 or 1) for a left or right facing fish and colour (a string). e.g.
-
-```
-fish(0, 'green')
-
-fish(colour='red')
-
-fish(index=1)
-```
-
-Currently available colours are (case insensitive): RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
-
-If an unknown colour or a non-colour string is provided, a random one will be chosen.
 
--- fishing.py
+++ fishing.py
@@ -1,43 +1,115 @@
+import random
+import artwork
 from colorama import init, deinit
 from colorama import Fore, Back, Style
-import random
 
+init(autoreset=True)
+NUM_QUESTIONS = 3
+                 
+class Game(object):
+    
+    def __init__(self):
+        self.level = 1
+        self.player = ""
+        self.level_score = 0
+        
+        self.ACTION_MAP = {1 : ["+", self.add, "magenta", artwork.SMALL_FISH],
+                  2 : ["-", self.subtract, "cyan", artwork.MEDIUM_FISH],
+                  3 : ["x", self.multiply, "yellow", artwork.FAT_FISH],
+                  4 : ["/", self.divide, "red", artwork.FAT_FISH]
+                 }
+    
+    def add(self, a, b):
+        return a + b
+    
+    def subtract(self, a, b):
+        return a - b
+    
+    def multiply(self, a, b):
+        return a * b
+    
+    def divide(self, a, b):
+        return a / b
+            
+    def choose_player(self):
+        initial = input("Choose your player.\n\nEnter E for Ellie or M for Mike\n")
+        initial = initial.upper()
+        players = {'E': 'Ellie',
+                   'M': 'Mike'}
+        self.player = players[initial]
+        print()
+        print("You are {}!".format(self.player))
+        artwork.print_picture(thing=artwork.PEOPLE[self.player])
+        
+        print("Here is your companion, Pingy Penguin!")
+        artwork.print_picture(thing=artwork.PENGUINS['big'])
+    
+    def play_level(self):
+        print(Back.RED + Fore.YELLOW + "LEVEL {}".format(self.level))
+        print()
+        input("Press enter to start.")
 
-COLOUR_MAP = {'RED': Fore.RED,
-              'GREEN': Fore.GREEN,
-              'BLUE': Fore.BLUE,
-              'YELLOW': Fore.YELLOW,
-              'MAGENTA': Fore.MAGENTA,
-              'CYAN': Fore.CYAN,
-          }
-          
-          
-def fish(index=None, colour=None):           
-    if index is None:
-        index = random.choice([0, 1])
-    if colour is None:
-        colour = random.choice(list(COLOUR_MAP.keys()))
-    
-    colour = colour.upper()
-    
-    colour = COLOUR_MAP.get(colour, Fore.WHITE)
-    
-    FISH_PICS = [
-        '''
-        |><>
-        ''',
-        '''
-        <><|
-        '''
-    ]
-    
-    print (colour + FISH_PICS[index])
-    print(Fore.RESET)
+        action_symbol = self.ACTION_MAP[self.level][0] 
+        action = self.ACTION_MAP[self.level][1]
+               
+        for i in range(1, NUM_QUESTIONS + 1):
+            
+            self._catch_fish()
+            
+            number1 = random.choice(range(1, 11))
+            number2 = random.choice(range(1, 11))
+ 
+            question = str(number1) + " " + action_symbol + " " + str(number2) + " = ?\n"
+            question_title = "Question " + str(i)
+            print(Back.YELLOW + Fore.BLUE + Style.BRIGHT + question_title) 
+            sum_answer = input(question)
 
-def main():
-    init()
-    fish()
-    deinit()
+            try:
+                if int(sum_answer) == action(number1, number2):
+                    print('Correct!')
+                    artwork.print_picture(thing=artwork.PEOPLE["correct_smiley"])
+                    self.level_score = self.level_score + 1
+                else:
+                    print('Incorrect! The right answer is', number1 + number2)
+                    print('Pingy eats your fish!')
+                    artwork.print_picture(thing=artwork.PENGUINS["eating"])
+                    print('Pingy is stuffed!')
+                    artwork.print_picture(thing=artwork.PENGUINS["fat"])
+            except ValueError:
+                print('Incorrect! The right answer is', number1 + number2)
+                print('Incorrect! The right answer is', number1 + number2)
+                print('Pingy eats your fish!')
+                artwork.print_picture(thing=artwork.PENGUINS["eating"])
+                print('Pingy is stuffed!')
+                artwork.print_picture(thing=artwork.PENGUINS["fat"])
+            print()
+            if i < NUM_QUESTIONS:
+                input("Press enter to fish for the next question.\n")
+            
+        if self.level_score == NUM_QUESTIONS:
+            print("PERFECT SCORE! YOU PASSED LEVEL {}!".format(self.level))
+            artwork.print_picture(thing=artwork.PEOPLE["winning_smiley"])
+        
+        if self.level < 4:
+            self.level += 1
+            self.level_score = 0
+            self.play_level()
+        
+    def _catch_fish(self):
+            
+        print("{} goes fishing!\n".format(self.player))
+        artwork.print_picture(colour=self.ACTION_MAP[self.level][2], 
+                              thing=self.ACTION_MAP[self.level][3][random.choice([0, 1])])
+        
+    
+game = Game()
+print("\n\n\n\n\n\n")
+print(Back.RED + Fore.YELLOW + "==========================================")
+print(Back.RED + Fore.YELLOW + "WELCOME TO HOLLY'S MATHS AND FISHING GAME!")
+print(Back.RED + Fore.YELLOW + "==========================================")
+print("\n\n")
 
-if __name__ == '__main__':
-    main()
+game.choose_player()
+game.play_level()
+
+deinit()
